e2c6c05d41e73c275bfd03b2902a629974b86ead
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-18 09:22:05 +0530

    access management identity verification system

--- FacialRecognitionSystem.py
+++ FacialRecognitionSystem.py
@@ -31,3 +31,25 @@ trainingDataFolder = "D:\\workspace\\facialytics\\training-data";
 metadataFile = "D:\\workspace\\facialytics\\training-data\\data.xlsx";
 
 knownFaceEncoding, knownFaceNames, metadata = loadDataFromFolder(trainingDataFolder, metadataFile)
+
+videoCapture = cv2.VideoCapture(0)
+while True:
+    ret, frame = videoCapture.read()
+    faceLocations = face_recognition.face_locations(frame)
+    faceEncodings = face_recognition.face_encodings(frame, faceLocations)
+
+    for faceEncoding in faceEncodings:
+        matches = face_recognition.compare_faces(knownFaceEncoding, faceEncoding)
+        name = "Unknown"
+        fullName = "Unknown"
+        age = "Unknown"
+        gender = "Unknown"
+
+        if True in matches:
+            firstMatchIndex = matches.index(True)
+            name = knownFaceNames[firstMatchIndex]
+            fullName, age, gender = metadata[firstMatchIndex]
+
+            cv2.putText(frame, "Identity verified, access granted", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 2555, 0), 2, cv2.LINE_AA)
+        else:
+            cv2.putText(frame, "Identity not verified, access denied", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 2555, 0), 2, cv2.LINE_AA)
